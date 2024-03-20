4ede0e4cee0ce0c73d50b3dab1cf6d77820189b9
abssourav <abubakkarsiddique1781@gmail.com>
2024-03-20 17:35:35 +0600

    initial

---
+++ Prettier Formatter/index.js
@@ -0,0 +1,23 @@
+const a = 'abs'
+const b = 'sourav'
+console.log(a + b)
+
+if (a) {
+   console.log(a)
+} else {
+   console.log()
+}
+
+function exampleFunction(longParameterName1, longParameterName) {
+   return longParameterName1 + longParameterName
+}
+
+function exampleFunction() {
+   if (true) {
+      console.log('Indented with spaces')
+   }
+}
+
+const func1 = a => {
+   console.log(a)
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## Prettier
+
+- For Code Formatting
+
+## ESLint
+
+- For Set of rules for coding
